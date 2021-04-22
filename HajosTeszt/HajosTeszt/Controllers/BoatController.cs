27cb4c982b68018642f5c70cf3da92aae64bb6b2
Ambrus Tóth <ambrus.toth@stud.uni-corvinus.hu>
2021-04-22 17:12:07 +0200

    REST API

--- HajosTeszt/HajosTeszt/Controllers/BoatController.cs
+++ HajosTeszt/HajosTeszt/Controllers/BoatController.cs
@@ -13,6 +13,19 @@ namespace HajosTeszt.Controllers
     public class BoatController : ControllerBase
     {
         [HttpGet]
+        [Route("questions/{sorszám}")]
+        public ActionResult M2(int sorszám)
+        {
+            HajostesztContext context = new HajostesztContext();
+            var kérdés = (from x in context.Questions
+                          where x.QuestionId == sorszám
+                          select x).FirstOrDefault();
+
+            if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
+
+            return new JsonResult(kérdés);
+        }
+        [HttpGet]
         [Route("questions/all")]
         public ActionResult M1()
         {
--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -1,43 +1,81 @@
 ﻿
 
-var kérdések;
-var o = 0;
+var aktualis;
+var o = 1;
 
 function letöltés() {
-    fetch('/questions.json')
+    /*fetch('/questions.json')
         .then(response => response.json())
         .then(data => letöltésBefejeződött(data)
+    );*/
+    fetch('/questions/1')
+        .then(response => response.json())
+        .then(data => kérdésMegjelenítés(data)
         );
 }
-function letöltésBefejeződött(d) {
+/*function letöltésBefejeződött(d) {
     console.log("Sikeres letöltés")
     console.log(d)
     kérdések = d;
     kérdésMegjelenítés(o);
-}
+}*/
 
 function kérdésMegjelenítés(kérdés) {
-    document.getElementById("kérdés_szöveg").innerHTML = kérdések[kérdés].questionText;
+    /*document.getElementById("kérdés_szöveg").innerHTML = kérdések[kérdés].questionText;
     document.getElementById("válasz1").innerHTML = kérdések[kérdés].answer1;
     document.getElementById("válasz2").innerHTML = kérdések[kérdés].answer2;
     document.getElementById("válasz3").innerHTML = kérdések[kérdés].answer3;
     if (kérdések[kérdés].image != "") {
         document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kérdések[kérdés].image;
     }
-    Clear();
+    Clear();*/
+    aktualis = kérdés;
+    console.log(kérdés);
+    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+    document.getElementById("válasz1").innerText = kérdés.answer1
+    document.getElementById("válasz2").innerText = kérdés.answer2
+    document.getElementById("válasz3").innerText = kérdés.answer3
+    if (kérdés.image != "") {
+        document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+    }
+    else
+    {
+        document.getElementById("kép").src = "";
+    }
+    
 }
 
 function Hátra() {
-    if (o == 0) {
+   
+    /*if (o == 0) {
         o = kérdések.length - 1;
         letöltés();
     }
     else {
         o--;
         letöltés();
+    }*/
+    if (o == 1) {
+        o = 859;
     }
-}
+    else {
+        o--;
+    }
+    kérdésBetöltés(o);
+    Clear();
+    }
+
 function Előre() {
+    if (o == 859) {
+        o = 1;
+    }
+    else {
+        o++;
+    }
+    kérdésBetöltés(o);
+    Clear();
+}
+    /*
     if (o == kérdések.length - 1) {
         o = 0;
         letöltés();
@@ -45,8 +83,8 @@ function Előre() {
     else {
         o++;
         letöltés();
-    }
-}
+    }*/
+
 
 
 
@@ -66,7 +104,7 @@ function válasz3Click() {
 }
 
 function valaszCheck(n) {
-    if (n == kérdések[o].correctAnswer) {
+    if (n == aktualis.correctAnswer) {
         document.getElementById("válasz" + n).classList.add("jo");
     }
     else {
@@ -82,3 +120,19 @@ function Clear() {
     document.getElementById("válasz3").classList.remove("jo");
     document.getElementById("válasz3").classList.remove("rossz");
 }
+
+
+
+
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => kérdésMegjelenítés(data));
+}    
